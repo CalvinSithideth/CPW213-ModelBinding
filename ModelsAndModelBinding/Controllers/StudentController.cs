3627217803ab494a5263750c46663ade2d4a0349
CalvinSithideth <calvin.sithideth@gmail.com>
2019-07-17 18:49:42 -0700

    Add project files.

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // When data is posted to /Student/Register
+        // this action will execute
+        [HttpPost]
+        public IActionResult Register(IFormCollection data)
+        {
+            // Validate input (lots of work!)
+            // Server-side and client-side
+            // Add error messages on page to notify user
+
+            Student stu = new Student()
+            {
+                FullName = data["full-name"],
+                ProgramOfChoice = data["program"],
+                HomeAddress = data["address"],
+                DateOfBirth = Convert.ToDateTime(data["dob"])
+            };
+
+            // Add to database
+            return View();
+        }
+
+        // Registration done with Model Binding!
+        [HttpGet]
+        public IActionResult RegisterMB()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterMB(Student s)
+        {
+            // This verifies all data annotations in
+            // the class are valid
+            if (ModelState.IsValid)
+            {
+                // Add to database
+                // Display success message
+            }
+            return View();
+        }
+    }
+}
---
+++ ModelsAndModelBinding/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsAndModelBinding.Models
+{
+    /// <summary>
+    /// Represents an individual student
+    /// </summary>
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        /// <summary>
+        /// The legal first and last name
+        /// ex. J Doe
+        /// </summary>
+        [Required(ErrorMessage = "Gimme that name!")] // Make required - change ErrMsg on view
+        [Display(Name = "Full Legal Name")] // Changes the label on a view
+        public string FullName { get; set; }
+
+        // Max 30 just for demo
+        [StringLength(30, MinimumLength = 5)] // Set max length of input
+        public string HomeAddress { get; set; }
+
+        /// <summary>
+        /// The program the student intends to
+        /// complete
+        /// </summary>
+        [Required]
+        [Display(Name = "Choose Program")]
+        public string ProgramOfChoice { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
